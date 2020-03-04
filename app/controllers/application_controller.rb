58a79e11355f42117b60d5a588b94b90e6c09d2b
github username <laurenkyu@gmail.com>
2020-03-04 16:25:59 -0500

    current user added to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
 
+    helper_method :current_user
+
+    def current_user  
+        if session[:user_id]
+        @current_user ||= User.find(session[:user_id])
+        else
+        @current_user = nil
+        end
+        return @current_user
+    end
+
     def homepage
     end
 end
